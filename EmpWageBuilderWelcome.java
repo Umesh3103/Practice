cb2a36dfc19f87b912adf51215c03aa592459a8b
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-22 17:50:38 +0530

    [Umesh3103]Added: checking employee is present or not

--- EmpWageBuilderWelcome.java
+++ EmpWageBuilderWelcome.java
@@ -1,5 +1,14 @@
 public class EmpWageBuilder{
 	public static void main(String[] args){
+		//Welcome message
 		System.out.println("Welcome to Employee wage problem statement");
+		//Employee present or not
+		int IS_FULL_TIME=1;
+		double empCheck= Math.floor(Math.random() *10)% 2;
+		if(empCheck == IS_FULL_TIME){
+			System.out.println("Employee is Present");
+		}
+		else
+			System.out.println("Employee is Absent");
 	}
 }
